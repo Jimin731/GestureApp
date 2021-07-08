7e45bb18c5bf65b98bf865090c29f75f083647aa
Jimin731 <petitestan@naver.com>
2021-07-08 16:54:50 +0900

    dragGesture

--- GestureApp/ContentView.swift
+++ GestureApp/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State var magnification = CGFloat(1.0)
+    @GestureState var offset = CGSize.zero
     var body: some View {
         let tapGesture = TapGesture(count: 2)
             .onEnded{_ in
@@ -26,14 +27,20 @@ struct ContentView: View {
             .onEnded{ value in
                 NSLog("MagGesture Ended : \(value)")
             }
+        let dragGesture = DragGesture()
+            .updating($offset){ dv, state, transcation in
+                state = dv.translation
+            }
         return VStack {
             Image(systemName: "hand.point.right.fill")
                 .font(.largeTitle)
                 .padding()
                 .border(Color.black, width: 2)
                 .scaleEffect(magnification)
-                .gesture(tapGesture)
-                .gesture(magGesture)
+                .offset(offset)
+                //.gesture(tapGesture)
+                //.gesture(magGesture)
+                .gesture(dragGesture)
         }
     }
 }
